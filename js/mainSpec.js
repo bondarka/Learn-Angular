8344a68718b58081a622ba0f63efe77ab9ee3433
Anastasiia Bondar <nastya.v.bondar@gmail.com>
2016-11-11 15:09:53 +0200

    add new task with filtr

---
+++ js/mainSpec.js
@@ -0,0 +1,10 @@
+describe("filter", function(){
+	beforeEach(module('app'));
+
+	describe("reverse", function(){
+		it("should be reverse a string", inject(function(reverseFilter){
+			expect(reverseFilter('ABCD')).toEqual('DCBA');
+			expect(reverseFilter('Anastasiia')).toEqual('aiisatsanA');
+		}))
+	})
+})
