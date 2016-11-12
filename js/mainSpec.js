2f10be8f631811c8bf9604a7773436cdae6f4eb8
Anastasiia Bondar <nastya.v.bondar@gmail.com>
2016-11-12 22:06:08 +0200

    Add filter and redesign

--- js/mainSpec.js
+++ js/mainSpec.js
@@ -1,10 +1,10 @@
-describe("filter", function(){
-	beforeEach(module('app'));
+// describe("filter", function(){
+// 	beforeEach(module('app'));
 
-	describe("reverse", function(){
-		it("should be reverse a string", inject(function(reverseFilter){
-			expect(reverseFilter('ABCD')).toEqual('DCBA');
-			expect(reverseFilter('Anastasiia')).toEqual('aiisatsanA');
-		}))
-	})
-})
+// 	describe("reverse", function(){
+// 		it("should be reverse a string", inject(function(reverseFilter){
+// 			expect(reverseFilter('ABCD')).toEqual('DCBA');
+// 			expect(reverseFilter('Anastasiia')).toEqual('aiisatsanA');
+// 		}))
+// 	})
+// })
