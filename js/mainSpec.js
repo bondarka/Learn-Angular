9af281464506c9c0eb1114ab229fe940682ecf6c
Anastasiia Bondar <nastya.v.bondar@gmail.com>
2016-11-13 23:59:50 +0200

    Add directive

--- js/mainSpec.js
+++ js/mainSpec.js
@@ -1,10 +1,28 @@
-// describe("filter", function(){
-// 	beforeEach(module('app'));
+var myApp = angular.module("myApp", [])
 
-// 	describe("reverse", function(){
-// 		it("should be reverse a string", inject(function(reverseFilter){
-// 			expect(reverseFilter('ABCD')).toEqual('DCBA');
-// 			expect(reverseFilter('Anastasiia')).toEqual('aiisatsanA');
-// 		}))
-// 	})
-// })
+myApp.directive("angulardir",function(){
+	return{
+		restrict:"A",
+		link: function(){
+			alert("superman I learn about directive. Now I use restrict C (only matches class name) and M (only matches comment)");
+		}
+	}
+})
+
+myApp.directive("welcome", function() {
+  return {
+    restrict: "A",
+    link: function(){
+      alert("Hello!");
+    }
+  }
+})
+
+myApp.directive("goodbye", function() {
+  return {
+    restrict: "A",
+    link: function(){
+      alert("See you later!");
+    }
+  }
+})
